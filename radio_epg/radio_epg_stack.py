f2a85dc40ed03cf4e014dfd1871d278a06084f41
roy by <roybenyosef@gmail.com>
2019-12-04 02:58:18 +0200

    Add out function

--- radio_epg/radio_epg_stack.py
+++ radio_epg/radio_epg_stack.py
@@ -3,7 +3,8 @@ from aws_cdk import (
     aws_events as events,
     aws_s3 as s3,
     aws_lambda as lambda_,
-    aws_events_targets as targets,)
+    aws_events_targets as targets, )
+from aws_cdk.core import Duration
 
 
 class RadioEpgStack(core.Stack):
@@ -11,11 +12,23 @@ class RadioEpgStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
+        data_bucket_lifecycle_rules = s3.LifecycleRule(noncurrent_version_expiration=Duration.days(3))
+        epg_data_s3_bucket_name = 'epg-data-s3-bucket-42'
         epg_data_s3_bucket = s3.Bucket(self,
-                                       'Bucket',
+                                       'EpgDataBucket',
+                                       bucket_name=epg_data_s3_bucket_name,
                                        versioned=True,
+                                       lifecycle_rules=[data_bucket_lifecycle_rules],
                                        removal_policy=core.RemovalPolicy.DESTROY)
-                                       #block_public_access=s3.BlockPublicAccess.BLOCK_ALL)
+        # block_public_access=s3.BlockPublicAccess.BLOCK_ALL)
+
+        epg_out_s3_bucket_name = 'epg-out-s3-bucket-42'
+        epg_out_s3_bucket = s3.Bucket(self,
+                                      'EpgOutBucket',
+                                      bucket_name=epg_out_s3_bucket_name,
+                                      versioned=False,
+                                      public_read_access=True,
+                                      removal_policy=core.RemovalPolicy.DESTROY)
 
         with open("show_updater_handler.py", encoding="utf8") as fp:
             handler_code = fp.read()
@@ -26,17 +39,18 @@ class RadioEpgStack(core.Stack):
             code=lambda_.InlineCode(handler_code),
             handler="index.main",
             runtime=lambda_.Runtime.PYTHON_3_7,
-            environment=dict(BUCKET_NAME=epg_data_s3_bucket.bucket_name)
+            environment=dict(DATA_BUCKET_NAME=epg_data_s3_bucket.bucket_name, OUT_BUCKET_NAME=epg_out_s3_bucket.bucket_name)
         )
 
         epg_data_s3_bucket.grant_read(lambda_fn)
+        epg_out_s3_bucket.grant_read_write(lambda_fn)
 
-        # Run every day at 6PM UTC
+        # Run every 5 minutes
         # See https://docs.aws.amazon.com/lambda/latest/dg/tutorial-scheduled-events-schedule-expressions.html
         rule = events.Rule(
             self, "Rule",
             schedule=events.Schedule.cron(
-                minute='0/1',
+                minute='0/5',
                 hour='*',
                 month='*',
                 week_day='*',
--- show_updater_handler.py
+++ show_updater_handler.py
@@ -8,8 +8,12 @@ S3 = boto3.client('s3')
 
 def main(event, context):
     try:
-        response = S3.get_object(Bucket=os.environ['BUCKET_NAME'], Key='epg_data.json')
+        response = S3.get_object(Bucket=os.environ['DATA_BUCKET_NAME'], Key='epg_data.json')
         file_content = json.loads(response['Body'].read())
         print(f'File content: {file_content}')
+
+        out_data = file_content[0]['epg']['Sun'][0]
+        S3.put_object(Body=out_data, Bucket=os.environ['OUT_BUCKET_NAME'], Key='epg.json')
+
     except ClientError as e:
         raise
